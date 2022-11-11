c2cebcfe48b9aee6ec71f8fd7cce0695313eedf1
DiamondHunters <13744511+DiamondHunters@users.noreply.github.com>
2022-11-11 18:51:16 +0800

    fix spell mistake
    #1 Thanks scyxdd

--- src/hooklog.js
+++ src/hooklog.js
@@ -19,7 +19,7 @@ let validator = {
     set: function (target, key, value) {
         if (key === 'log') {
             log('console.log override blocked');
-            rerturn;
+            return;
         }
         target[key] = value;
     }
